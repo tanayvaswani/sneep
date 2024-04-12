72805a38f6dd311c843fde166c35def9d441f425
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 01:06:39 +0530

    chore: user & profile page

---
+++ app/(platform)/(routes)/users/[userId]/page.tsx
@@ -0,0 +1,9 @@
+const UserProfilePage = () => {
+    return ( 
+        <div>
+            Profile page based on userId
+        </div>
+     );
+}
+ 
+export default UserProfilePage;
---
+++ app/(platform)/(routes)/users/page.tsx
@@ -0,0 +1,9 @@
+const UsersPage = () => {
+    return (
+        <div>
+            Shows all available user
+        </div>
+     );
+}
+
+export default UsersPage;
