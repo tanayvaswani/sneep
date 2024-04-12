25aa4db423cc4cf843c95f1f3293a486b8bbe940
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 01:17:32 +0530

    feat: user profile page

--- app/(platform)/(routes)/users/[userId]/page.tsx
+++ app/(platform)/(routes)/users/[userId]/page.tsx
@@ -1,9 +1,25 @@
-const UserProfilePage = () => {
-    return ( 
-        <div>
-            Profile page based on userId
-        </div>
-     );
-}
- 
+import prisma from '@/lib/prisma';
+
+const getUser = async (userId: number) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  return user;
+};
+
+const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
+  const user = await getUser(Number(params.userId));
+
+  return (
+    <div>
+      <div>{user?.email}</div>
+      <div>{user?.firstName}</div>
+      <div>{user?.lastName}</div>
+    </div>
+  );
+};
+
 export default UserProfilePage;
