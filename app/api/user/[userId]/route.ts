eedc0268ab3be91de56ed114cad54c68f8756f05
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-12 20:39:29 +0530

    feat: user login API

--- app/api/user/[userId]/route.ts
+++ app/api/user/[userId]/route.ts
@@ -25,3 +25,38 @@ export async function GET(
     console.error('Error fetching user: ', error);
   }
 }
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const { username, email, password, firstName, lastName } = await body;
+
+  if (!username || !email || !password || !firstName || !lastName) {
+    return NextResponse.json({ message: 'All fields are mandatory' });
+  }
+
+  const existingUserCheck = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingUserCheck) {
+    return NextResponse.json({ message: 'Email might already be in use.' });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      username: username,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    },
+  });
+
+  return NextResponse.json(
+    { message: 'User Added!', user: newUser },
+    { status: 200 },
+  );
+}
