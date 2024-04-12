c694e76911cfe39516c723da86bc0bf6f2616b3f
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-12 20:18:46 +0530

    fix: error handling

--- app/api/user/[userId]/route.ts
+++ app/api/user/[userId]/route.ts
@@ -6,11 +6,19 @@ export async function GET(
   request: Request,
   { params }: { params: { userId: string } },
 ) {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: Number(params.userId),
-    },
-  });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(params.userId),
+      },
+    });
 
-  return NextResponse.json({ user });
+    if (!user) {
+      return NextResponse.json({ message: 'User not found!' });
+    }
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error('Error fetching user: ', error);
+  }
 }
