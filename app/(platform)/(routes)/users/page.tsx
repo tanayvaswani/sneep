0bd2bb9882ec9e1b05320930f397333593ecb963
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 01:09:28 +0530

    feat: users fetching on users page

--- app/(platform)/(routes)/users/page.tsx
+++ app/(platform)/(routes)/users/page.tsx
@@ -1,9 +1,26 @@
-const UsersPage = () => {
-    return (
-        <div>
-            Shows all available user
+import prisma from '@/lib/prisma';
+
+const getUsers = async () => {
+  const users = await prisma.user.findMany({});
+
+  return users;
+};
+
+const UsersPage = async () => {
+  const users = await getUsers();
+
+  return (
+    <div>
+      {users?.map((user, index) => (
+        <div key={index}>
+          <div>
+            {user?.firstName} {user?.lastName}
+          </div>
+          <div>{user?.email}</div>
         </div>
-     );
-}
+      ))}
+    </div>
+  );
+};
 
 export default UsersPage;
