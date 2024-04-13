73d36bdd11ca1bd6f6d1eee592763e444569480d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 21:23:53 +0530

    fix: diaglog

--- components/new-post.tsx
+++ components/new-post.tsx
@@ -10,17 +10,25 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
 
 // TODO: MAKE THIS A FORM
 const NewPost = () => {
   return (
     <Dialog>
-      <DialogTrigger>New Post</DialogTrigger>
+      <DialogTrigger>
+        <Button>New Post</Button>
+      </DialogTrigger>
 
       <DialogContent>
-        <DialogHeader className='flex flex-col items-center justify-center w-full'>
-          <DialogTitle>Create new post</DialogTitle>
+        <DialogHeader className="flex w-full flex-col items-center justify-center">
+          <DialogTitle className="space-y-1 text-center">
+            <h1 className="text-base text-zinc-200 md:text-xl">Create New Post</h1>
+            <p className="text-sm text-zinc-400 md:text-base">
+              Fill all the details below to create a post
+            </p>
+          </DialogTitle>
 
           <DialogDescription className="flex w-full flex-col items-center justify-center gap-4 pt-6">
             <div className="w-full space-y-1">
@@ -30,10 +38,12 @@ const NewPost = () => {
 
             <div className="w-full space-y-1">
               <Label>Code</Label>
-              <Input className="bg-zinc-900"  />
+              <Textarea className="bg-zinc-900" rows={15} />
             </div>
           </DialogDescription>
         </DialogHeader>
+
+        <Button>Post</Button>
       </DialogContent>
     </Dialog>
   );
