28d3a14186f7dc55fc49b5c2f4b92cdc10698345
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 15:22:14 +0530

    feat: user page

--- app/(platform)/(routes)/users/page.tsx
+++ app/(platform)/(routes)/users/page.tsx
@@ -1,4 +1,7 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import prisma from '@/lib/prisma';
+import { User2Icon } from 'lucide-react';
+import Link from 'next/link';
 
 const getUsers = async () => {
   const users = await prisma.user.findMany({});
@@ -10,14 +13,17 @@ const UsersPage = async () => {
   const users = await getUsers();
 
   return (
-    <div>
+    <div className="grid w-full grid-cols-1 items-center justify-center gap-4 px-4 py-16 md:grid-cols-4">
       {users?.map((user, index) => (
-        <div key={index}>
-          <div>
-            {user?.firstName} {user?.lastName}
-          </div>
-          <div>{user?.email}</div>
-        </div>
+        <Link href={`/users/${user?.id}`} key={index}>
+          <Card className="flex flex-col items-center justify-center bg-zinc-950">
+            <CardHeader className="flex items-center gap-2 text-2xl">
+              <User2Icon /> {user?.firstName} {user?.lastName}
+            </CardHeader>
+
+            <CardContent>{user?.email}</CardContent>
+          </Card>
+        </Link>
       ))}
     </div>
   );
