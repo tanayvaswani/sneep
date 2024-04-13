b4bb6f4c9fbfb557ddb445f3450618ef4f8acb77
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-14 01:01:14 +0530

    fix: homepage UI

--- app/(platform)/(routes)/home/page.tsx
+++ app/(platform)/(routes)/home/page.tsx
@@ -1,5 +1,14 @@
 import NewPost from '@/components/new-post';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import prisma from '@/lib/prisma';
+import { ArrowBigDown, ArrowBigUp } from 'lucide-react';
 
 const getPosts = async () => {
   const posts = await prisma.post.findMany();
@@ -11,13 +20,28 @@ const HomePage = async () => {
   const posts = await getPosts();
 
   return (
-    <div>
+    <div className="grid grid-cols-1 items-center gap-6 py-16 md:mx-auto md:max-w-screen-md">
       {posts?.map((post, index) => (
-        <div key={index}>
-          <div>{post?.code}</div>
-          <div>{post?.description}</div>
-          <div>{post?.authorId}</div>
-        </div>
+        <Card
+          key={index}
+          className="rounded-none border-none border-zinc-400 bg-transparent"
+        >
+          <CardHeader>
+            <CardTitle>{post.description}</CardTitle>
+          </CardHeader>
+
+          <CardContent>{post.code}</CardContent>
+
+          <CardFooter className="flex items-center justify-around border-b">
+            <Button className="flex items-center">
+              <ArrowBigUp /> {post.upvotes}
+            </Button>
+
+            <Button className="flex items-center">
+              <ArrowBigDown /> {post.downvotes}
+            </Button>
+          </CardFooter>
+        </Card>
       ))}
 
       <NewPost />
