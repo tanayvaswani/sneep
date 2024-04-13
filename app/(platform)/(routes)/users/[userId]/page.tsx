f19143a0a08caccd444303b8a57aee1b6243d5a4
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 15:10:22 +0530

    feat: profile page

--- app/(platform)/(routes)/users/[userId]/page.tsx
+++ app/(platform)/(routes)/users/[userId]/page.tsx
@@ -22,9 +22,15 @@ const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
             <User2Icon className="h-16 w-16 p-4 text-neutral-300 md:h-32 md:w-32 md:p-8" />
           </div>
 
-          <h1 className="text-xl font-semibold text-zinc-300 md:text-3xl">
-            {user?.firstName} {user?.lastName}
-          </h1>
+          <div className="flex flex-col">
+            <h1 className="text-xl font-semibold text-zinc-300 md:text-3xl">
+              {user?.firstName} {user?.lastName}
+            </h1>
+
+            <h2 className="text-base font-semibold text-zinc-500 md:text-lg">
+              @{user?.username}
+            </h2>
+          </div>
         </div>
 
         <div className="flex items-center gap-2 text-sm text-zinc-400 md:text-base">
@@ -32,6 +38,7 @@ const UserProfilePage = async ({ params }: { params: { userId: string } }) => {
         </div>
       </div>
 
+      {/* TODO: Add users posts */}
       <div></div>
     </div>
   );
