3d50e70880a736464ad40f52ac9e2ff22d78098d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-13 21:08:51 +0530

    chore: todo added

--- components/new-post.tsx
+++ components/new-post.tsx
@@ -11,6 +11,8 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+
+// TODO: MAKE THIS A FORM
 const NewPost = () => {
   return (
     <Dialog>
