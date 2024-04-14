ce56c0216eaee03f616cf258de928638c8bc0675
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-15 00:02:45 +0530

    chore: added next-auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,26 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
+
+        const pwHash = saltAndHashPassword(credentials.password);
+
+        user = await getUserFromDb(credentials.email, pwHash);
+
+        if (!user) {
+          throw new Error('User not found.');
+        }
+
+        return user;
+      },
+    }),
+  ],
 });
